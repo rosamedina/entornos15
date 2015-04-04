557ee292326927f53624da3ec7ae72daa177463c
rosamedina <rosa@iessanvicente.com>
2015-04-04 20:03:51 +0200

    Update EntornosGIT.java

--- entornosGIT/src/entornosgit/EntornosGIT.java
+++ entornosGIT/src/entornosgit/EntornosGIT.java
@@ -10,7 +10,7 @@ public class EntornosGIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Primeros pasos con GIT en Eclipse");
+        System.out.println("Primeros pasos con GIT en Eclipse!!");
         System.out.println("Netbeans mola más, menudo asco de eclipse");
     }    
 }
