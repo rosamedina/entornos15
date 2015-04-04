633133464ce78916f9c2a15452e536442c747c95
rosam <rosa@iessanvicente.com>
2015-04-04 19:12:20 +0200

    Primeros pasos con GIT y Netbeans

--- entornosGIT/src/entornosgit/EntornosGIT.java
+++ entornosGIT/src/entornosgit/EntornosGIT.java
@@ -15,7 +15,7 @@ public class EntornosGIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Primeros pasos con GIT!");
     }
     
 }
