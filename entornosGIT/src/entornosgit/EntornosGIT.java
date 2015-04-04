df5240ef597836247274df57ce3e86fc5cf872e1
rosamedina <rosa@iessanvicente.com>
2015-04-04 19:28:27 +0200

    Update EntornosGIT.java
    Modificando los comentarios de la clase

--- entornosGIT/src/entornosgit/EntornosGIT.java
+++ entornosGIT/src/entornosgit/EntornosGIT.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entornosgit;
-
 /**
  *
  * @author Rosa Medina
@@ -12,6 +6,7 @@ package entornosgit;
 public class EntornosGIT {
 
     /**
+     * Programa principal donde veremos los cambios efectuados desde el github con Netbeans
      * @param args the command line arguments
      */
     public static void main(String[] args) {
